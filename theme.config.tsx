155ebc1942f00324c9e8a5cc1afbb80f55f3b3ef
Ashton Brown <ashton.brown@hmdchi-09k9nqcw.companynet.org>
2023-10-27 12:53:02 -0500

    Created placeholders for the sidebar component.

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>AB Docs
+  </span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/mr7brown',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://www.linkedin.com/in/ashton-brown-mba-6000238a/',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // footer: {
+  //   text: 'Nextra Docs Template',
+  // },
 }
 
 export default config
